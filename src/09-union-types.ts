3ceb2e4538a37756852d86dde2eee58b849b9ddd
Pablo <amepablo@gmail.com>
2022-06-29 11:49:39 -0300

    Clase 14: Union Types

---
+++ src/09-union-types.ts
@@ -0,0 +1,16 @@
+(() => {
+  let userId: string | number | boolean;
+  userId = 1212;
+  userId = 'asasa' ;
+
+  // El tipado se vuelve dinamico depende el resultado de la sentencia
+  function greeting(myText: string | number) {
+    if(typeof myText === 'string') {
+      console.log(`string ${myText.toLowerCase()}`);
+    } else {
+      console.log(`number ${myText.toFixed(1)}`);
+    }
+  }
+  greeting('asas');
+  greeting(123.3254325235);
+})();
